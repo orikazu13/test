95d42c58c1e5e2bf1da7dfdadd291ff26a60be95
orikazu13 <k.orikanai.sys19@morijyobi.ac.jp>
2020-06-26 12:37:15 +0900

    aaa

---
+++ insert.php
@@ -0,0 +1,23 @@
+<?php
+$dsn = 'mysql:dbname=test_DB;host=localhost;';
+$user = 'kazuto';
+$password = 'kazu1313';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+
+    $sql = "insert into user values (:id, :name, :age)";
+    $stmt = $dbh->prepare($sql);
+    $prams = array(':id'=> $id, ':name'=> $name, ':age'=> $age,);
+    $stmt->execute($prams);
+
+    header('Location: index.php?fg=1');
+    
+} catch (PDOException $e) {
+    print "Failed: " . $e->getMessage() . "\n";
+    exit();
+?>
