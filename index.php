c57160775d6b568cb2d6f4367f2dc6f56750f956
orikazu13 <k.orikanai.sys19@morijyobi.ac.jp>
2020-06-29 12:18:50 +0900

    q

--- index.php
+++ index.php
@@ -42,12 +42,24 @@ try {
             <p class="read">sampleのやつやぞ<br>aaaaaaaaaaaS</p>
         </div>
 	</div>
-	
-	<?php if($_GET['fg'] == 1) { ?>
-		<p>Success</p>
-	<?php } else if ($_GET['fg'] == 2) { ?>
-		<p>Failed</p>
-	<?php } ?>
+	<div class="container">
+		<?php if ($_GET['fg'] == 1) { ?>
+			<div class="alert alert-success alert-dismissible fade show" role="alert">
+				Processing complete. <strong>Success!!</strong>
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+		<?php } else if ($_GET['fg'] == 2) { ?>
+			<div class="alert alert-danger alert-dismissible fade show" role="alert">
+				Processing complete. <strong>Failed!!</strong>
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+		<?php } ?>
+	</div>
+
     <div class="container">
 		<ul class="nav nav-tabs">
 			<li class="nav-item">
